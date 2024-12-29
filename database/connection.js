a65bd363483007263bf9c42a3f22553245b4575f
neamatullahmustafa <tareqshrouq@gmail.com>
2024-12-29 18:07:46 +0200

    first

--- database/connection.js
+++ database/connection.js
@@ -1,37 +1,14 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 
 const dbConnection = async () => {
-  const connectionString = "mongodb://localhost:27017/sarahaApp";
-
   try {
-    const connection = await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const connection = await connect("mongodb://localhost:27017/sarahaApp");
     console.log(
       `Connected to database successfully: ${connection.connection.host}`
     );
   } catch (error) {
     console.error(`Error while connecting to database: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
-
-  // Handle MongoDB connection events
-  mongoose.connection.on("disconnected", () => {
-    console.warn("MongoDB disconnected!");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.error(`MongoDB connection error: ${err}`);
-  });
 };
-
-// Graceful shutdown for the database connection
-process.on("SIGINT", async () => {
-  await mongoose.connection.close();
-  console.log("MongoDB connection closed due to app termination");
-  process.exit(0);
-});
-
 export default dbConnection;
